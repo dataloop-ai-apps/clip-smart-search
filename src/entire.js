87ac8da773d96d1f99ce385f7e19167195e5eded
Or Shabtay <or@dataloop.ai>
2023-09-19 22:42:24 +0300

    single script

--- src/entire.js
+++ src/entire.js
@@ -1,60 +1,65 @@
 async function run(textInput, itemsQuery) {
-    let tokenizer
-    let textModel
-    let transformers
+    try {
+        let tokenizer
+        let textModel
+        let transformers
 
-    async function loadDependencies() {
-        try {
-            let quantized = false; // change to `true` for a much smaller model (e.g. 87mb vs 345mb for image model), but lower accuracy
-            transformers = await import('https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.1')
+        async function loadDependencies() {
+            try {
+                let quantized = false; // change to `true` for a much smaller model (e.g. 87mb vs 345mb for image model), but lower accuracy
+                transformers = await import('https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.1')
+                transformers.env.allowLocalModels = false
+                transformers.env.remoteHost = "https://huggingface.co/"
+                transformers.env.remotePathTemplate = "Xenova/clip-vit-base-patch32/resolve/main"
 
-            transformers.env.remoteHost = "https://huggingface.co/"
-            transformers.env.remotePathTemplate = "Xenova/clip-vit-base-patch32/resolve/main"
+                tokenizer = await transformers.AutoTokenizer.from_pretrained('')
+                textModel = await transformers.CLIPTextModelWithProjection.from_pretrained('dummy', { quantized });
 
-            tokenizer = await transformers.AutoTokenizer.from_pretrained('dummy')
-            textModel = await transformers.CLIPTextModelWithProjection.from_pretrained('dummy', { quantized });
+            }
+            catch (e) {
+                console.log(e)
+            }
 
         }
-        catch (e) {
-            console.log(e)
-        }
+        await loadDependencies()
 
-    }
-    await loadDependencies()
+        // console.log('done')
+        // get text embedding:
+        let texts = ['cigarettes on the sidewalk'];
+        let textInputs = tokenizer(texts, { padding: true, truncation: true });
+        let { text_embeds } = await textModel(textInputs);
+        let vector = text_embeds.data
+        // console.log(textInputs);
+        console.log(vector);
 
-    // console.log('done')
-    // get text embedding:
-    let texts = ['cigarettes on the sidewalk'];
-    let textInputs = tokenizer(texts, { padding: true, truncation: true });
-    let { text_embeds } = await textModel(textInputs);
-    let vector = text_embeds.data
-    // console.log(textInputs);
-    console.log(vector);
-
-    let query = {
-        filter: { $and: [{ hidden: false }, { type: 'file' }] },
-        page: 0,
-        pageSize: 1000,
-        resource: 'items',
-        join: {
-            on: {
-                resource: 'feature_vectors',
-                local: 'entityId',
-                forigen: 'id'
-            },
-            filter: {
-                value: {
-                    $euclid: {
-                        input: vector,
-                        $euclidSort: { eu_dist: 'ascending' }
-                    }
+        let query = {
+            filter: { $and: [{ hidden: false }, { type: 'file' }] },
+            page: 0,
+            pageSize: 1000,
+            resource: 'items',
+            join: {
+                on: {
+                    resource: 'feature_vectors',
+                    local: 'entityId',
+                    forigen: 'id'
                 },
-                featureSetId: 'feature_set.id'
+                filter: {
+                    value: {
+                        $euclid: {
+                            input: vector,
+                            $euclidSort: { eu_dist: 'ascending' }
+                        }
+                    },
+                    featureSetId: 'feature_set.id'
+                }
             }
         }
-    }
 
-    return query
+        return query
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
 
 
